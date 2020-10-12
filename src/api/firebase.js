62a9a887d10823c81468808c6668c89468a8725c
kiendv97 <dangquangvu999@gmail.com>
2020-10-12 09:27:58 +0700

    unscriber device token when logout

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,9 +1,25 @@
-const fireBaseUrl = 'https://iid.googleapis.com/iid/v1:batchAdd'
+const fireBaseUrlAdd = 'https://iid.googleapis.com/iid/v1:batchAdd'
+const fireBaseUrlRemove = 'https://iid.googleapis.com/iid/v1:batchRemove'
 const axios = require('axios')
 const serverKey = 'key=AAAA6XZOpOk:APA91bElHLg770foPiGClB7Fb__Q3FwQL1ks3UduRtg6lKc5gdPbbxsCdt90XzPcwjEf7MsSVRNNckuQANuPOJUTSQ0rYpP2HtxaNTshppB241FHQTYaLnW6bDreOrxhXmr0M4AqeyYj'
 let postDevice = async (nameTopic) => {
     console.log(nameTopic);
-    let result = await axios.post(`${fireBaseUrl}`,
+    let result = await axios.post(`${fireBaseUrlAdd}`,
+        {
+            to: `/topics/appgiaosim${nameTopic}`,
+            registration_tokens: [`${localStorage.tokenDevice}`]
+        },
+        {
+            headers: {
+                "Authorization": serverKey,
+                "Content-Type": 'application/json'
+            }
+        })
+    return result.data.response;
+}
+let unsubcribeDevice = async (nameTopic) => {
+    console.log(nameTopic);
+    let result = await axios.post(`${fireBaseUrlRemove}`,
         {
             to: `/topics/appgiaosim${nameTopic}`,
             registration_tokens: [`${localStorage.tokenDevice}`]
@@ -17,5 +33,6 @@ let postDevice = async (nameTopic) => {
     return result.data.response;
 }
 export {
-    postDevice
+    postDevice,
+    unsubcribeDevice
 }
--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,58 +1,71 @@
 <template>
-<v-layout row>
+  <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
-        <v-card style="height: 100vh" class="ma-0">
-            <v-toolbar color="#00B7C2" dark class="ma-0">
-                <v-btn icon @click="$router.go(-1)">
-                    <v-icon>arrow_back</v-icon>
+      <v-card style="height: 100vh" class="ma-0">
+        <v-toolbar color="#00B7C2" dark class="ma-0">
+          <v-btn icon @click="$router.go(-1)">
+            <v-icon>arrow_back</v-icon>
+          </v-btn>
+
+          <v-toolbar-title>Thông tin cá nhân</v-toolbar-title>
+
+          <v-spacer />
+        </v-toolbar>
+
+        <v-list two-line>
+          <template style="pa-1" v-for="(item, index) in items">
+            <v-subheader v-if="item.header" :key="item.header">
+              {{ item.header }}
+            </v-subheader>
+
+            <v-divider v-else-if="item.divider" :key="index" />
+
+            <v-list-tile
+              v-else
+              :key="item.title"
+              avatar
+              @click="canClick(item)"
+            >
+              <v-icon>
+                {{ item.avatar }}
+              </v-icon>
+
+              <v-list-tile-content class="ml-3">
+                <v-list-tile-title v-html="item.title" />
+                <v-list-tile-sub-title
+                  class="font-weight-bold"
+                  v-html="item.subtitle"
+                />
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+          <v-dialog v-model="dialog" max-width="300">
+            <v-card>
+              <v-card-title class="body-1 font-weight-bold">
+                Đăng xuất
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer />
+
+                <v-btn outline color="green darken-1" text @click="logout()">
+                  Đăng xuất
                 </v-btn>
 
-                <v-toolbar-title>Thông tin cá nhân</v-toolbar-title>
-
-                <v-spacer />
-            </v-toolbar>
-
-            <v-list two-line>
-                <template style="pa-1" v-for="(item, index) in items">
-                    <v-subheader v-if="item.header" :key="item.header">
-                        {{ item.header }}
-                    </v-subheader>
-
-                    <v-divider v-else-if="item.divider" :key="index" />
-
-                    <v-list-tile v-else :key="item.title" avatar @click="canClick(item)">
-                        <v-icon>
-                            {{ item.avatar }}
-                        </v-icon>
-
-                        <v-list-tile-content class="ml-3">
-                            <v-list-tile-title v-html="item.title" />
-                            <v-list-tile-sub-title class="font-weight-bold" v-html="item.subtitle" />
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
-                <v-dialog v-model="dialog" max-width="300">
-                    <v-card>
-                        <v-card-title class="body-1 font-weight-bold">
-                            Đăng xuất
-                        </v-card-title>
-                        <v-card-actions>
-                            <v-spacer />
-
-                            <v-btn outline color="green darken-1" text @click="logout()">
-                                Đăng xuất
-                            </v-btn>
-
-                            <v-btn outline color="red darken-1" text @click="dialog = false">
-                                Huỷ
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-            </v-list>
-        </v-card>
+                <v-btn
+                  outline
+                  color="red darken-1"
+                  text
+                  @click="dialog = false"
+                >
+                  Huỷ
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-list>
+      </v-card>
     </v-flex>
-</v-layout>
+  </v-layout>
 </template>
 
 <style scoped>
@@ -60,89 +73,96 @@
 
 <script>
 import axios from "axios";
+import { unsubcribeDevice } from "@/api/firebase";
 export default {
-    data() {
-        return {
-            dialog: false,
-            profile: {},
-            items: [{
-                    header: "Thông tin"
-                },
-                {
-                    avatar: "accessibility",
-                    title: " Tên cá nhân",
-                    subtitle: " "
-                },
-                {
-                    divider: true,
-                    inset: true
-                },
-                {
-                    avatar: "email",
-                    title: ' <span class=" text--lighten-1"> Email</span>',
-                    subtitle: " "
-                },
-                {
-                    divider: true,
-                    inset: true
-                },
-                {
-                    avatar: "phone",
-                    title: "Phone",
-                    subtitle: ""
-                },
-                {
-                    divider: true,
-                    inset: true
-                },
-                {
-                    avatar: "arrow_forward_ios",
-                    title: "Đổi mật khẩu",
-                    linkTo: "/change-password",
-                    canClick: true
-                },
-                {
-                    divider: true,
-                    inset: true
-                },
-                {
-                    avatar: "arrow_forward_ios",
-                    title: "Đăng xuất",
-                    dialogShow: true,
-                    canClick: true
-                }
-            ]
-        };
-    },
-    watch: {
-        profile: function (val) {
-            this.items[1].subtitle = val.full_name;
-            this.items[3].subtitle = val.email;
-            this.items[5].subtitle = `<span class='text--primary'>${val.phone_number}</span> `;
-        }
-    },
-    async created() {
-        let profileLocal = JSON.parse(localStorage.getItem('user'));
-
-        this.profile = profileLocal;
-
-    },
-    methods: {
-        logout() {
-            localStorage.removeItem('user');
-            localStorage.removeItem('access_token');
-            this.$router.push('login');
+  data() {
+    return {
+      dialog: false,
+      profile: {},
+      items: [
+        {
+          header: "Thông tin",
         },
-        canClick(item) {
-            if (item.canClick) {
-                if (item.linkTo) {
-                    location.href = item.linkTo;
-                }
-                if (item.dialogShow) {
-                    this.dialog = true;
-                }
-            }
+        {
+          avatar: "accessibility",
+          title: " Tên cá nhân",
+          subtitle: " ",
+        },
+        {
+          divider: true,
+          inset: true,
+        },
+        {
+          avatar: "email",
+          title: ' <span class=" text--lighten-1"> Email</span>',
+          subtitle: " ",
+        },
+        {
+          divider: true,
+          inset: true,
+        },
+        {
+          avatar: "phone",
+          title: "Phone",
+          subtitle: "",
+        },
+        {
+          divider: true,
+          inset: true,
+        },
+        {
+          avatar: "arrow_forward_ios",
+          title: "Đổi mật khẩu",
+          linkTo: "/change-password",
+          canClick: true,
+        },
+        {
+          divider: true,
+          inset: true,
+        },
+        {
+          avatar: "arrow_forward_ios",
+          title: "Đăng xuất",
+          dialogShow: true,
+          canClick: true,
+        },
+      ],
+    };
+  },
+  watch: {
+    profile: function (val) {
+      this.items[1].subtitle = val.full_name;
+      this.items[3].subtitle = val.email;
+      this.items[5].subtitle = `<span class='text--primary'>${val.phone_number}</span> `;
+    },
+  },
+  async created() {
+    let profileLocal = JSON.parse(localStorage.getItem("user"));
+
+    this.profile = profileLocal;
+  },
+  methods: {
+    async logout() {
+      await unsubcribeDevice(this.profile.username);
+      localStorage.removeItem("user");
+      localStorage.removeItem("access_token");
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (let registration of registrations) {
+          registration.unregister();
         }
-    }
+      });
+      this.$router.push("login");
+    },
+    canClick(item) {
+      if (item.canClick) {
+        if (item.linkTo) {
+          location.href = item.linkTo;
+        }
+        if (item.dialogShow) {
+          this.dialog = true;
+        }
+      }
+    },
+  },
 };
 </script>
